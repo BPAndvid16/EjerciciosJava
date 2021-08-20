8f27ca4eb770439dd97cdb7573e0a80accd6ff4d
BPAndvid16 <biomedicalpeople@gmail.com>
2021-08-19 19:57:17 -0500

    Reto 3

--- Auto.java
+++ Auto.java
@@ -8,25 +8,34 @@ public class Auto {
     public Auto (String marca, int tipo){
         this.marca = marca;
         this.tipo = tipo;
+        
     }
 
     
      
     public static double Bono (Auto auto){
-        double ganancia = 0;
-        switch(auto.tipo)
+        int ganancia = 0;
+        int caso =  auto.tipo;
+
+
+        switch(caso)
         {
             case 1:
-                ganancia += 750000;
+                ganancia = 750000;
+                break;
                 
             case 2:
-                ganancia += 500000;
+                ganancia = 500000;
+                break;
                 
             case 3:
-                ganancia += 350000;
+                ganancia = 350000;
+                break;
                 
 
         }
+
+       
         
         return ganancia;
     }
--- Empleado.java
+++ Empleado.java
@@ -6,8 +6,8 @@ public class Empleado {
     private final String nombre;
     private final String apellido;
     private static ArrayList<Auto> autos = new ArrayList<>();
+    private static  int salario ;
     
-    private final int salario;
     static double nomina = 0.0;
 
     
@@ -20,14 +20,34 @@ public class Empleado {
         
         this.salario = salario;
         
-
+       
         
+    }
+
+    public void setAutos(ArrayList<Auto> autos)
+    {
+        this.autos = autos;
 
     }
 
-    public static double calcularMiNomina(Empleado empleado, Auto auto){
+    
+
+
+    public static double calcularMiNomina(Empleado empleado){
+
+        int saldoAutos = 0;
+        salario = 1875000;
+        empleado.setAutos(autos);
         
-        nomina = empleado.salario + Auto.Bono(auto);
+
+        for (int i = 0; i < autos.size(); i++) {
+
+            saldoAutos += Auto.Bono(autos.get(i));
+            System.out.println(Auto.Bono(autos.get(i)));
+
+          }
+        
+        nomina = empleado.salario + saldoAutos;
         double total = nomina - 2 * (nomina * 0.04);
         
         
@@ -37,7 +57,11 @@ public class Empleado {
 
     
 
+    
+
     public static void main(String[] args) {
+
+        
 
         Scanner usuario = new Scanner(System.in);
 
@@ -51,24 +75,50 @@ public class Empleado {
         String apellido = usuario.nextLine();
 
 
+        Boolean autoSelect = true;
+       
+
+        while(autoSelect){
+
         System.out.println("Digite la marca: ");
 
         String marca = usuario.nextLine();
 
-       
-        int salario = 1875000;
+        System.out.println("Seleccione el tipo de auto: ");
+        System.out.println("1. Deportivo o camioneta ");
+        System.out.println("2. Auto eléctrico ");
+        System.out.println("3. Auto tradicional");
 
+        int tipoAuto = usuario.nextInt();
+
+        Auto userAutos = new Auto(marca, tipoAuto);
+        autos.add(userAutos); 
         
         
 
+        System.out.println("¿Ha adquirido otro vehiculo?");
+        System.out.println("1. Si");
+        System.out.println("2. No");
 
-        Empleado Liquidacion = new Empleado(nombre, apellido,salario);
-        Auto carac = new Auto(marca, 1);
+        int autoBucle = usuario.nextInt();
+        if (autoBucle == 1){
+
+            autoSelect = true;
+        } else {
+
+           autoSelect = false;
+        }
+    
+
+        }
+           
+           
+           Empleado Liquidacion = new Empleado(nombre, apellido,salario);
+           System.out.print("El salario de " + Liquidacion.nombre + Liquidacion.apellido + " es de " + calcularMiNomina(Liquidacion) );
+           autoSelect = false;
+           usuario.close();
+
         
-        System.out.println("El salario de " + Liquidacion.nombre + Liquidacion.apellido +  " es " + calcularMiNomina(Liquidacion,carac));
-        usuario.close();
-
-        System.out.println(autos);
 
         
     }
